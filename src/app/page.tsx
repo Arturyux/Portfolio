d5d61d04c63bbaada1a29dbb7178c5f7fa91b374
Arturyux <artur.burlakin@gmail.com>
2025-08-06 20:15:16 +0200

    admin button added to test in production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { JSX, useState } from 'react';
+import Link  from 'next/link';
 
 type SectionKey = 'home' | 'about' | 'projects' | 'contact';
 
@@ -11,37 +12,23 @@ export default function Home() {
     home: (
       <div className="p-6">
         <h2 className="text-3xl font-bold mb-4">Welcome to My Portfolio</h2>
-        {/* <p className="text-lg text-gray-700">
-          Hello! I&apos;m Artur Burlakin, a Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        </p> */}
       </div>
     ),
     about: (
       <div className="p-6">
         <h2 className="text-3xl font-bold mb-4">About Me</h2>
-        {/* <p className="text-lg text-gray-700">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p> */}
       </div>
     ),
     projects: (
       <div className="p-6">
         <h2 className="text-3xl font-bold mb-4">Projects</h2>
         <ul className="list-disc list-inside text-lg text-gray-700">
-          {/* <li>Project</li>
-          <li>Project</li>
-          <li>Project</li> */}
         </ul>
       </div>
     ),
     contact: (
       <div className="p-6">
         <h2 className="text-3xl font-bold mb-4">Contact</h2>
-        {/* <p className="text-lg text-gray-700">
-          Email: artur.burlakin@gmail.com
-        </p>
-        <p className="text-lg text-gray-700">LinkedIn</p>
-        <p className="text-lg text-gray-700">GitHub</p> */}
       </div>
     ),
   };
@@ -54,7 +41,6 @@ export default function Home() {
         </h1>
       </header>
       <div className="flex flex-1 flex-col sm:flex-row">
-        {/* Left Sidebar */}
         <nav className="bg-gray-100 w-full sm:w-64 p-6 border-r border-gray-200">
           <ul className="space-y-4">
             <li>
@@ -104,6 +90,13 @@ export default function Home() {
               >
                 Contact
               </button>
+            </li>
+            <li>
+              <Link href="/admin">
+                <button className="w-full px-4 py-2 text-left rounded bg-gray-500 text-white hover:bg-gray-600">
+                  Admin
+                </button>
+              </Link>
             </li>
           </ul>
         </nav>
